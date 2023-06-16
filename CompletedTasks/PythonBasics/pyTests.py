72337980f176257580ec82b7330aac69c4969d88
Lorenzo Zaidowicz <zaidowiczlorenzo@gmail.com>
2023-06-16 16:23:10 -0400

    Added folders for Basic Java and Python + PyLibraries

---
+++ CompletedTasks/PythonBasics/pyTests.py
@@ -0,0 +1,34 @@
+#useful py syntax
+import math
+import time
+
+
+#name = "1"
+#name = int(name) Cast in py
+
+
+#Slicing creates substrings [start=index:stop:step=order]
+
+#name = "Otravigus"
+#reversedName = name[::-1] 
+#print(reversedName)
+
+#   slice
+#website = "http://google.com"
+#slice = slice(7,-4)
+#print(website[slice])
+
+# if statments in py , indentation important
+#age = int(input("How old are you:"))
+
+#if age >=21:
+#    print("Access Granted")
+#else:
+#    print("Error: Invalid age")
+
+# logic in py (and, or , not)
+#if age >21 and age <30 or age == 20:
+ #   print("Young")
+#elif not(age < 60):
+ #   print("old")
+
